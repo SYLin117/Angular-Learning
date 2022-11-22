65ecf9ebd7077cfa39190968b8c8d4e8adc2d57f
Ian Lin <newianlin@gmail.com>
2022-11-22 12:02:10 -0800

    Using Subject to replace EventEmitter

--- course-project/src/app/shopping-list/shopping-list.service.ts
+++ course-project/src/app/shopping-list/shopping-list.service.ts
@@ -1,4 +1,5 @@
 import { Injectable, EventEmitter } from '@angular/core';
+import { Subject } from 'rxjs';
 import { Ingredient } from '../shared/ingredient.model';
 
 @Injectable({
@@ -6,7 +7,10 @@ import { Ingredient } from '../shared/ingredient.model';
 })
 export class ShoppingListService {
 
-  ingredientsChanged = new EventEmitter<Ingredient[]>();
+  // ingredientsChanged = new EventEmitter<Ingredient[]>();
+  ingredientsChanged = new Subject<Ingredient[]>();
+
+
   private ingredients: Ingredient[] = [
     new Ingredient('Apples', 5),
     new Ingredient('Tomatoes', 10),
@@ -19,14 +23,16 @@ export class ShoppingListService {
 
   addIngredient(ingredient: Ingredient) {
     this.ingredients.push(ingredient)
-    this.ingredientsChanged.emit(this.ingredients.slice())
+    // this.ingredientsChanged.emit(this.ingredients.slice())
+    this.ingredientsChanged.next(this.ingredients.slice())
     // console.log('new ingredient emit')
     // console.log(`ingredients: ${JSON.stringify(this.ingredients)}`)
   }
 
   addIngredients(ingredients: Ingredient[]) {
     this.ingredients.push(...ingredients)
-    this.ingredientsChanged.emit(this.ingredients.slice())
+    // this.ingredientsChanged.emit(this.ingredients.slice())
+    this.ingredientsChanged.next(this.ingredients.slice())
     console.log('slService ingredients:' + JSON.stringify(this.ingredients))
   }
 }
