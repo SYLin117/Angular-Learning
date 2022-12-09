66d5e1f5cd7aced2b67e2dd40c9d6d5289b9f521
Ian Lin <newianlin@gmail.com>
2022-12-08 19:46:16 -0800

    change api key

--- course-project/src/environments/environment.ts
+++ course-project/src/environments/environment.ts
@@ -3,7 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  API_KEY: 'AIzaSyAF3x0UDQJgT55wUsbyrjGEoyezdU74Xy4'
 };
 
 /*
